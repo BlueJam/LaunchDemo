0ea6d9ba9472109be38c2f71a81763687f1e4f9a
BlueJam <bimrose@gmail.com>
2013-10-31 14:33:10 +0000

    Initial commit of the demo, uploading everything needed.

---
+++ LaunchDemo/src/uk/co/bimrose/android/launchdemo/MainActivity.java
@@ -0,0 +1,32 @@
+package uk.co.bimrose.android.launchdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+	
+	private EditText lat;
+	private EditText lon;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		lat = (EditText)findViewById(R.id.lat);
+		lon = (EditText) findViewById(R.id.lon);
+	}
+	
+	public void showMe(View v){
+		String _lat = lat.getText().toString();
+		String _lon = lon.getText().toString();
+		Uri uri = Uri.parse("geo:"+_lat+","+_lon+"?z=15");
+		
+		startActivity(new Intent(Intent.ACTION_VIEW, uri));
+	}
+
+}
